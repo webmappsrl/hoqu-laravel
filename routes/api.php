8a87caa33ff08d511ecb4cd250bd8166b283be80
gianmarx <gianmarcogagliardi@webmapp.it>
2020-09-07 10:24:53 +0200

    change route add,pull,update

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('queues','QueuesController@index');
-Route::post('queues','QueuesController@add');
-Route::put('queuesPull','QueuesController@pull');
-Route::put('queuesUpdate','QueuesController@update');
+Route::get('/','QueuesController@index');
+Route::post('/','QueuesController@add');
+Route::put('pull','QueuesController@pull');
+Route::put('update','QueuesController@update');
 
 
 
