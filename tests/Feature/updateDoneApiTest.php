16487bd2a91862491b10906ee3b127a2d747d806
gianmarx <gianmarcogagliardi@webmapp.it>
2020-10-09 16:09:15 +0200

    develop test updateDone

--- tests/Feature/updateDoneApiTest.php
+++ tests/Feature/updateDoneApiTest.php
@@ -77,4 +77,193 @@ class updateDoneApiTest extends TestCase
         $response->assertStatus(403);
     }
 
+
+    public function testIdNotExistUpdateDoneApiHoqu()
+    {
+        Task::truncate();
+        $user_tokens = json_decode(Storage::get('test_data/tokens_users.json'),TRUE);
+        //add data with api/store
+        $data = [
+            "instance" => "https:\/\/montepisanotree.org",
+            "job" => "task1",
+            "parameters" => ["a"=> "yes", "b"=> "no", "c" => "so and so", "d"=>["poi"=>02,"route"=>2345]],
+        ];
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['instance@webmapp.it']
+        ])->post('/api/store',$data);
+        $response->assertStatus(201);
+
+        //logout user
+        $this->resetAuth();
+
+        //request that sends the "requesting server"
+        $requestSvr1 = [
+            "id_server" => 10,
+            "task_available" => ["task1","mptupdatepoi", "mptupdatetrack", "mptupdateroute", "mptdeleteroute","mptdeletepoi"],
+        ];
+        //OPERATIONS 1
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/pull',$requestSvr1);
+        $response->assertStatus(200);
+
+        //get value elaborate by pull
+        $dataDbTest = $response;
+        //request that sends the "requesting server 2"
+        $requestSvr2 = [
+            "id_server" => 10,
+            "task_available" => ["task1","mptupdatepoi", "mptupdatetrack", "mptupdateroute", "mptdeleteroute","mptdeletepoi"],
+            "id_task" => 100,
+        ];
+        //OPERATIONS 2
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/updateDone',$requestSvr2);
+        $dataDbTestUp = $response;
+        $response->assertStatus(400);
+    }
+
+
+
+    public function testCheckValueStatusUpdateDoneApiHoqu()
+    {
+        Task::truncate();
+        $user_tokens = json_decode(Storage::get('test_data/tokens_users.json'),TRUE);
+
+        //add data with api/store
+        $data = [
+            "instance" => "https:\/\/montepisanotree.org",
+            "job" => "task1",
+            "parameters" => ["a"=> "yes", "b"=> "no", "c" => "so and so", "d"=>["poi"=>02,"route"=>2345]],
+        ];
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['instance@webmapp.it']
+        ])->post('/api/store',$data);
+        $response->assertStatus(201);
+
+        //logout user
+        $this->resetAuth();
+
+        //request that sends the "requesting server"
+        $requestSvr1 = [
+            "id_server" => 10,
+            "task_available" => ["task1","mptupdatepoi", "mptupdatetrack", "mptupdateroute", "mptdeleteroute","mptdeletepoi"]
+        ];
+
+        //OPERATIONS 1
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/pull',$requestSvr1);
+        $response ->assertStatus(200);
+        //get value elaborate by pull
+        $dataDbTest = $response;
+        //request that sends the "requesting server 2"
+        $requestSvr2 = [
+            "id_server" => 10,
+            "status" => '',
+            "log" => "log test",
+            "id_task" => $dataDbTest['id'],
+        ];
+
+        //OPERATIONS 2
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/updateDone',$requestSvr2);
+        //get value elaborated by update
+        $dataDbTestUp = $response;
+        $response->assertStatus(400);
+
+        //request that sends the "requesting server 2"
+        $requestSvr1 = [
+            "id_server" => 10,
+            "status" => 'duplicate',
+            "log" => "log test",
+            "id_task" => $dataDbTest['id'],
+        ];
+        //OPERATIONS 3
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/updateDone',$requestSvr2);
+        //get value elaborated by update
+        $response->assertStatus(400);
+
+        //request that sends the "requesting server 2"
+        $requestSvr1 = [
+            "id_server" => 10,
+            "log" => "log test",
+            "id_task" => $dataDbTest['id'],
+        ];
+        //OPERATIONS 4
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/updateDone',$requestSvr2);
+        $response->assertStatus(400);
+    }
+
+
+public function testDoneUpdateDoneApiHoqu()
+    {
+        Task::truncate();
+        $user_tokens = json_decode(Storage::get('test_data/tokens_users.json'),TRUE);
+
+        //add data with api/queues
+        $data = [
+            "instance" => "https:\/\/montepisanotree.org",
+            "job" => "task1",
+            "parameters" => ["a"=> "yes", "b"=> "no", "c" => "so and so", "d"=>["poi"=>02,"route"=>2345]],
+        ];
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['instance@webmapp.it']
+        ])->post('/api/store',$data);
+        $response->assertStatus(201);
+
+        //logout user
+        $this->resetAuth();
+
+        //request that sends the "requesting server"
+        $requestSvr1 = [
+            "id_server" => 10,
+            "task_available" => ["task1","mptupdatepoi", "mptupdatetrack", "mptupdateroute", "mptdeleteroute","mptdeletepoi"],
+        ];
+        //OPERATIONS 1
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/pull',$requestSvr1);
+        $response ->assertStatus(200);
+        //get value elaborate by pull
+        $dataDbTest = $response;
+        //request that sends the "requesting server 2"
+        $requestSvr2 = [
+            "id_server" => 10,
+            "status" => "done",
+            "log" => "log test",
+            "id_task" => $dataDbTest['id'],
+        ];
+        //OPERATIONS 2
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$user_tokens['server@webmapp.it']
+        ])->put('/api/updateDone',$requestSvr2);
+        //get value elaborated by update
+        $ja = Task::find($response['id']);
+        $response ->assertStatus(200);
+        $this->assertSame('done',$ja['process_status']);
+        $this->assertSame($requestSvr2['log'],$ja['process_log']);
+        $this->assertSame($requestSvr2['id_server'],$ja['id_server']);
+        $this->assertSame($data['instance'],$ja['instance']);
+        $this->assertSame(json_decode($response['parameters'],TRUE),json_decode($ja['parameters'],TRUE));
+        $this->assertSame($data['parameters'],json_decode($ja['parameters'],TRUE));
+        $this->assertSame($response['id'],$ja["id"]);
+    }
+
 }
