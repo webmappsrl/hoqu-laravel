29eebf28c5408154980095b1dcd9b16f843f1ac7
gianmarx <gianmarcogagliardi@webmapp.it>
2020-11-11 10:29:22 +0100

    filter todo

--- app/Http/Livewire/TableTodo.php
+++ app/Http/Livewire/TableTodo.php
@@ -1,93 +1,107 @@
 <?php
 
 namespace App\Http\Livewire;
-use Livewire\WithPagination;
+    use Livewire\WithPagination;
 
-use App\Models\Task;
-use Livewire\Component;
+    use App\Models\Task;
+    use Livewire\Component;
 
-class TableTodo extends Component
-{
-    use WithPagination;
-
-    public $instance;
-    public $job;
-    public $jobs;
-
-    public $tasks;
-    public $instances;
-
-    public function updatingSearch()
+    class TableTodo extends Component
     {
-        $this->resetPage();
-    }
+        use WithPagination;
 
-    public function mount()
-    {
-        $this->instance='';
-        $this->job='';
-        $this->tasks=Task::whereIn('process_status', ['new','processing'])->orderByRaw('FIELD(process_status, "new", "processing")asc')->orderBy('created_at', 'asc')->get();
-        $this->instances=[];
-        $this->jobs=[];
-    }
+        public $instance;
+        public $job;
+        public $jobs;
+        public $countJ = 0;
+        public $countI = 1;
 
-    public function updatedinstance()
-    {
-        $in = Task::find($this->instance);
+        public $instances;
 
-        if(!empty($in))
+        public function updatinginstance()
         {
-            $this->tasks= Task::whereIn('process_status', ['new','processing'])
-            ->where('instance','like', $in->instance)
-            ->orderByRaw('FIELD(process_status, "new", "processing")asc')
-            ->orderBy('created_at', 'asc')
-            ->get()
-            ;
+            $this->resetPage();
         }
-        else
+
+        public function mount($instance,$job)
         {
-            $this->tasks= Task::whereIn('process_status', ['new','processing'])
-            ->orderByRaw('FIELD(process_status, "new", "processing")asc')
-            ->orderBy('created_at', 'asc')
-            ->get()
-            ;
+            $this->instance=$instance;
+            $this->job=$job;
+            // $this->tasks=[];
+            $this->instances=[];
+            $this->jobs=[];
+        }
+
+        public function updatedinstance()
+        {
+            $in = Task::find($this->instance);
+
+            if(!empty($in))
+            {
+                $tasks= Task::whereIn('process_status', ['new','processing'])
+                ->where('instance','like', $in->instance)
+                ->orderByRaw('FIELD(process_status, "new", "processing")asc')
+                ->orderBy('created_at', 'asc')
+                ->paginate(50)
+                ;
+            }
+            else
+            {
+                $tasks= Task::whereIn('process_status', ['new','processing'])
+                ->orderByRaw('FIELD(process_status, "new", "processing")asc')
+                ->orderBy('created_at', 'asc')
+                ->paginate(50)
+                ;
+            }
+            $this->countJ=0;
+            $this->countI=1;
+            return $tasks;
+
+        }
+
+        public function updatedjob()
+        {
+
+            $in_job = Task::find($this->job);
+
+            if(!empty($in_job))
+            {
+                $tasks = Task::whereIn('process_status', ['new','processing'])
+                ->where('job', 'like', $in_job->job)
+                ->orderByRaw('FIELD(process_status, "new", "processing")asc')
+                ->orderBy('created_at', 'asc')
+                ->paginate(50);
+            }
+            else
+            {
+                $tasks = Task::whereIn('process_status', ['new','processing'])
+                ->orderByRaw('FIELD(process_status, "new", "processing")asc')
+                ->orderBy('created_at', 'asc')
+                ->paginate(50);
+            }
+            $this->countJ=1;
+            $this->countI=0;
+            return $tasks;
+        }
+
+
+        public function render()
+        {
+            $this->instances = Task::whereIn('process_status', ['new','processing'])->orderBy('created_at', 'asc')->get();
 
             $this->jobs = Task::whereIn('process_status', ['new','processing'])->orderBy('created_at', 'asc')->get();
+
+            if($this->countJ == 1 && $this->countI == 0)
+            {
+                $this->countJ = 0;
+                $tasks=$this->updatedjob();
+            }
+            if($this->countI == 1 && $this->countJ == 0)
+            {
+                $this->countI = 0;
+                $tasks=$this->updatedinstance();
+            }
+
+            return view('livewire.table-todo',['tasks' => $tasks]);
         }
-
     }
-
-    public function updatedjob()
-    {
-
-        $in_job = Task::find($this->job);
-
-        if(!empty($in_job))
-        {
-            $this->tasks = Task::whereIn('process_status', ['new','processing'])
-            ->where('job', 'like', $in_job->job)
-            ->orderByRaw('FIELD(process_status, "new", "processing")asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-        }
-        else
-        {
-            $this->tasks = Task::whereIn('process_status', ['new','processing'])
-            ->orderByRaw('FIELD(process_status, "new", "processing")asc')
-            ->orderBy('created_at', 'asc')
-            ->get();
-        }
-
-        $this->instances = Task::whereIn('process_status', ['new','processing'])->orderBy('created_at', 'asc')->get();
-
-    }
-
-
-    public function render()
-    {
-        $this->instances = Task::whereIn('process_status', ['new','processing'])->orderBy('created_at', 'asc')->get();
-
-        $this->jobs = Task::whereIn('process_status', ['new','processing'])->orderBy('created_at', 'asc')->get();
-        return view('livewire.table-todo',['posts' => Task::whereIn('process_status', ['new','processing'])->paginate(50)]);
-    }
-}
--- resources/views/livewire/table-todo.blade.php
+++ resources/views/livewire/table-todo.blade.php
@@ -115,7 +115,7 @@
                    </tbody>
                 </table>
                 <ul class="w-7/12 px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-                    {{-- {{ $tasks->links() }} --}}
+                    {{ $tasks->links() }}
                 </ul>
              </div>
           </div>
