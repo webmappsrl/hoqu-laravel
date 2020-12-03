b110999f087433d55336186464fe434c6b107034
gianmarx <gianmarcogagliardi@webmapp.it>
2020-12-03 17:40:08 +0100

    fix color delete

--- resources/views/livewire/delete.blade.php
+++ resources/views/livewire/delete.blade.php
@@ -22,7 +22,7 @@
         </div>
         <div id="buttonChange" class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-              <button id="delB" wire:click.prevent="delete({{$this->Task_id}})" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+              <button id="delB" wire:click.prevent="delete({{$this->Task_id}})" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 Delete
               </button>
             </span>
