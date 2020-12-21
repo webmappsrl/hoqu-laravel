62607d59264205592a3942c661b180e678437624
gianmarx <gianmarcogagliardi@webmapp.it>
2020-12-21 17:48:44 +0100

    card backlog

--- resources/views/livewire/status-show.blade.php
+++ resources/views/livewire/status-show.blade.php
@@ -16,30 +16,40 @@
                 @if($isTrello)
                     @include('livewire.trello')
                 @endif
+                @if($isTrelloBug)
+                    @include('livewire.trelloBug')
+                @endif
                 @if($isReschedule)
                     @include('livewire.resk')
                 @endif
             <div class="container m-auto p-8 text-grey-darkest">
                 @if($task->process_status=='error' || $task->process_status=='done' )
                     <div class="flex flex-wrap -mx-2 mb-4">
-                        <div class="w-full sm:1/3 md:w-1/3 lg:w-1/3 px-2 mb-4">
+                        <div class="w-full sm:1/4 md:w-1/4 lg:w-1/4 px-2 mb-4">
                             <div class="h-12 text-sm text-grey-dark px-1 mb-1 flex items-center justify-center shadow-sm rounded-md bg-white">
                                 <button id="editRes" data-toggle="modal" data-target="#updateModal" wire:click="editRes({{$task}})" class="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500  hover:text-white shadow-md py-2 px-6 inline-flex items-center">
                                     <span class="mr-1">Reschedule</span>
                                 </button>
                             </div>
                         </div>
-                        <div class="w-full sm:1/3 md:w-1/3 lg:w-1/3 px-2 mb-4">
+                        <div class="w-full sm:1/4 md:w-1/4 lg:w-1/4 px-2 mb-4">
                             <div class="h-12 text-sm text-grey-dark px-1 mb-1 flex items-center justify-center shadow-sm rounded-md bg-white">
                                 <button id="editShow" data-toggle="modal" data-target="#updateModal" wire:click="edit({{$task}})" class="bg-white text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500  hover:text-white shadow-md py-2 px-6 inline-flex items-center">
                                     <span class="mr-1">Skip</span>
                                 </button>
                             </div>
                         </div>
-                        <div class="w-full sm:1/3 md:w-1/3 lg:w-1/3 px-2 mb-4">
+                        <div class="w-full sm:1/4 md:w-1/4 lg:w-1/4 px-2 mb-4">
                             <div class="h-12 text-sm text-grey-dark px-1 mb-1 flex items-center justify-center shadow-sm rounded-md bg-white">
                                 <button id="editCard" data-toggle="modal" data-target="#updateModal" wire:click="setCardTrello({{$task}})" class="bg-white text-gray-800 font-bold rounded border-b-2 border-pink-500 hover:border-pink-600 hover:bg-pink-500  hover:text-white shadow-md py-2 px-6 inline-flex items-center">
                                     <span class="mr-1">Card</span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="w-full sm:1/4 md:w-1/4 lg:w-1/4 px-2 mb-4">
+                            <div class="h-12 text-sm text-grey-dark px-1 mb-1 flex items-center justify-center shadow-sm rounded-md bg-white">
+                                <button id="editCard" data-toggle="modal" data-target="#updateModal" wire:click="setCardTrelloBug({{$task}})" class="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500  hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                                    <span class="mr-1">Bug</span>
                                 </button>
                             </div>
                         </div>
