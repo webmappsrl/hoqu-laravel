d3eb0bc2e18b9a42ddcf9e136fe203007d067dce
gianmarx <gianmarcogagliardi@webmapp.it>
2021-01-29 10:14:56 +0100

    api wmserver

--- app/Http/Controllers/WmServerController.php
+++ app/Http/Controllers/WmServerController.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\Wm_Server;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class WmServerController extends Controller
 {
+    public function get_server(Request $request)
+    {
+        if($request->user()->tokenCan('read'))
+        {
+            $servers = Wm_Server::all()->map->toArray();
+            $d = [];
+            $dr = [];
+            foreach ($servers as $server)
+            {
+                $task_processing = Task::where('id_server',$server['server_id'])->count();
+                $a = ['processing'=>$task_processing];
+                $d  += array_merge($server,$a);
+                $dr += $d;
+            }
+
+
+
+            return response()->json($dr,200);
+
+        }
+        else return abort(403,'Unauthorized');
+    }
 
 }
