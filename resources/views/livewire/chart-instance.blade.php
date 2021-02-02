327bf820ca92a325c30887a7ea5bbd92a6e13908
gianmarx <gianmarcogagliardi@webmapp.it>
2021-02-02 16:46:33 +0100

    fix 500 by view

--- resources/views/livewire/chart-instance.blade.php
+++ resources/views/livewire/chart-instance.blade.php
@@ -3,41 +3,41 @@
                     <canvas id="canvas2" width="800" height="650"></canvas>
                 </div>
 
-<script>
+{{--<script>--}}
 
 
-    window.addEventListener('load', (event) => {
-        var hour = <?php echo $hour; ?>;
-    var job = <?php echo $job; ?>;
-    var barChartData = {
-        labels: hour,
-        datasets: [{
-            label: 'Instance',
-            backgroundColor: ["indingo","red","yellow","blue","purple","orange","cyan","brown","pink","olive"],
-            data: job
-        }]
-    };
-        var ctx = document.getElementById("canvas2");
-        window.myBar = new Chart(ctx, {
-            type: 'pie',
-            data: barChartData,
-            options: {
+{{--    window.addEventListener('load', (event) => {--}}
+{{--        var hour = <?php echo $hour; ?>;--}}
+{{--    var job = <?php echo $job; ?>;--}}
+{{--    var barChartData = {--}}
+{{--        labels: <?php echo $percentage; ?>,--}}
+{{--        datasets: [{--}}
+{{--            label: 'Instance',--}}
+{{--            backgroundColor: ["indingo","red","yellow","blue","purple","orange","cyan","brown","pink","olive"],--}}
+{{--            data: job--}}
+{{--        }]--}}
+{{--    };--}}
+{{--        var ctx = document.getElementById("canvas2");--}}
+{{--        window.myBar = new Chart(ctx, {--}}
+{{--            type: 'pie',--}}
+{{--            data: barChartData,--}}
+{{--            options: {--}}
 
-                elements: {
-                    rectangle: {
-                        borderWidth: 2,
-                        borderColor: '#c1c1c1',
-                        borderSkipped: 'bottom'
-                    }
-                },
-                responsive: true,
-                title: {
-                    display: true,
-                    text: 'instance'
-                }
-            }
-        });
-    });
+{{--                elements: {--}}
+{{--                    rectangle: {--}}
+{{--                        borderWidth: 2,--}}
+{{--                        borderColor: '#c1c1c1',--}}
+{{--                        borderSkipped: 'bottom'--}}
+{{--                    }--}}
+{{--                },--}}
+{{--                responsive: true,--}}
+{{--                title: {--}}
+{{--                    display: true,--}}
+{{--                    text: 'instance'--}}
+{{--                }--}}
+{{--            }--}}
+{{--        });--}}
+{{--    });--}}
 
-</script>
+{{--</script>--}}
 
